92960b334a9e4965053b94ba7ddeeffd672e1dab
Jasmyn Hicks <jasmynh44@gmail.com>
2023-10-21 12:38:11 -0400

    Add condition to check if user input is a valid max number

--- Guessing_Game/app.js
+++ Guessing_Game/app.js
@@ -1,9 +1,25 @@
-console.log("sanity check")
-
 let maxNum = parseInt(prompt("Enter a high number"));
+while(!maxNum) {
+  prompt("Enter a valid number or press 'q' to quit")
+}
 
 let targetNum = Math.floor(Math.random() * maxNum) + 1;
 
-console.log(targetNum)
-let guess = prompt("Enter your first guess")
+console.log(`Target Number: ${targetNum}`);
 
+let guess = prompt("Enter your first guess or type 'q' to quit");
+
+let attempts = 1;
+
+while(parseInt(guess) !== targetNum) {
+  if(guess === 'q') break; 
+  if(guess > targetNum) {
+    guess = prompt("That's too high, guess again") 
+    attempts++
+  } else if(guess < targetNum) {
+    guess = prompt("That's too low, guess a gain")
+    attempts++
+  } else {
+    
+  }
+}
