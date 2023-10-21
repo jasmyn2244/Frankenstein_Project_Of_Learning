756aa31ad8acbf7cb943c25e2f1f7939561331ca
Jasmyn Hicks <jasmynh44@gmail.com>
2023-10-21 12:55:00 -0400

    Remove console.log and refactor

--- Guessing_Game/app.js
+++ Guessing_Game/app.js
@@ -1,22 +1,20 @@
 let maxNum = parseInt(prompt("Enter a high number"));
-while(!maxNum) {
+while (!maxNum) {
   maxNum = parseInt(prompt("Enter a valid high number or press 'q' to quit"));
 }
 
 let targetNum = Math.floor(Math.random() * maxNum) + 1;
 
-console.log(`Target Number: ${targetNum}`);
-
 let guess = prompt("Enter your first guess or type 'q' to quit");
 
 let attempts = 1;
 
-while(parseInt(guess) !== targetNum) {
-  if(guess === 'q') break; 
-  if(guess > targetNum) {
+while (parseInt(guess) !== targetNum) {
+  if (guess === 'q') break; 
+  if (guess > targetNum) {
     guess = prompt("That's too high, guess again") 
     attempts++
-  } else if(guess < targetNum) {
+  } else if (guess < targetNum) {
     guess = prompt("That's too low, guess a gain")
     attempts++
   } else {
@@ -24,7 +22,7 @@ while(parseInt(guess) !== targetNum) {
   }
 }
 
-if(guess === 'q') {
+if (guess === 'q') {
   console.log("Ok, you quit. Hit refresh to start a new game")
 } else {
   console.log(`CONGRATS! You guessed the correct number! It took you ${attempts} guesses`)
