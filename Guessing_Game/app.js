2b49ec8ed051a5bd2666829eb707270f2c4418c2
Jasmyn Hicks <jasmynh44@gmail.com>
2023-10-21 12:16:48 -0400

    Reorganize files

---
+++ Guessing_Game/app.js
@@ -0,0 +1,9 @@
+console.log("sanity check")
+
+let maxNum = parseInt(prompt("Enter a high number"));
+
+let targetNum = Math.floor(Math.random() * maxNum) + 1;
+
+console.log(targetNum)
+let guess = prompt("Enter your first guess")
+
