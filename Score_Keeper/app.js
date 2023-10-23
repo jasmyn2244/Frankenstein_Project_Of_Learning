c69d105ca6b746b6cccea097a69ee151ab65d59a
Jasmyn Hicks <jasmynh44@gmail.com>
2023-10-23 18:12:40 -0400

    Add functionality to buttons to increment score

--- Score_Keeper/app.js
+++ Score_Keeper/app.js
@@ -1,6 +1,8 @@
 //console.log('testing')
 
-const score = document.querySelector('#score');
+const scoreboard = document.querySelector('#scoreBoard');
+const scoreboard1 = document.querySelector('#player1Score');
+const scoreboard2 = document.querySelector('#player2Score');
 const btnPlayer1 = document.querySelector('#player1');
 const btnPlayer2 = document.querySelector('#player2');
 const btnReset = document.querySelector('#reset');
@@ -13,16 +15,22 @@ let gameOver = false
 
 
 btnReset.addEventListener('click', function () {
-  player1Score = 0;
-  player2Score = 0;
+  player1Score.innerText = 0;
+  player2Score.innerText = 0;
 })
 
+// function updateScore() {
+//   scoreboard.innerText = `${player1Score} to ${player2Score}`;
+// }
+
 function updateScore() {
-  score.innerText = `${player1Score} to ${player2Score}`;
+  scoreboard1.innerText = `${player1Score}`;
+  scoreboard2.innerText = `${player2Score}`;
 }
 
-while(player1Score < playingTo.value && player2Score < playingTo.value) {
-btnPlayer1.addEventListener('click', function () {
+// while(player1Score < playingTo.value && player2Score < playingTo.value) {
+
+  btnPlayer1.addEventListener('click', function () {
   player1Score++;
   updateScore();
 })
@@ -31,14 +39,14 @@ btnPlayer2.addEventListener('click', function () {
   player2Score++;
   updateScore();
 })
-}
+// }
 
-if(player1Score > player2Score) {
-  player1Score.classList('winner');
-  player2Score.classList('loser');
-} else {
-  player1Score.classList('winner');
-  player2Score.classList('loser');
-}
+// if(player1Score > player2Score) {
+//   player1Score.classList.add('winner');
+//   player2Score.classList.add('loser');
+// } else {
+//   player1Score.classList.add('winner');
+//   player2Score.classList.add('loser');
+// }
 
 
