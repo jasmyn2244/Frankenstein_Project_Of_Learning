54a0acc5c1f49579fa1c1f646a1746cb8ed1f2e6
Jasmyn Hicks <jasmynh44@gmail.com>
2023-11-02 15:18:18 -0400

    Require langs package and use it to transtale our language code into a language string

--- Language_Guesser/index.js
+++ Language_Guesser/index.js
@@ -1,4 +1,10 @@
 const franc = require('franc');
+const langs = require('langs');
 
 const langCode = franc('Wie geht es dir');
-console.log(langCode)
+
+if (langCode === 'und') {
+  console.log('Sorry, we can\'t translate that. Try a longer sentence')
+} else {
+  console.log(langs.where('3', langCode).name);
+}
