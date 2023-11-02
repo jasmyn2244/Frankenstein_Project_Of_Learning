10680a2471dc48f8a54f48e3c3ed6d3bf56929df
Jasmyn Hicks <jasmynh44@gmail.com>
2023-11-02 15:25:53 -0400

    Add a way to handle errors for unidentified language codes

--- Language_Guesser/index.js
+++ Language_Guesser/index.js
@@ -6,5 +6,11 @@ const langCode = franc('Wie geht es dir');
 if (langCode === 'und') {
   console.log('Sorry, we can\'t translate that. Try a longer sentence')
 } else {
-  console.log(langs.where('3', langCode).name);
+  const language = langs.where('3', langCode).name;
+  if (!language) {
+    console.log('Sorry, we had a problem deciphering this language, try another language');
+  } else {
+  console.log(language)
+  }
 }
+
